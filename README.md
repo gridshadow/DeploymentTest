352adb77d43da16c4b58fac3697351b6b3a3fa19
Scott Congreve <congreve@karlin.mff.cuni.cz>
2023-03-12 15:03:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deployment test
+
+A test repository to test GitHub Actions to do the following:
+
+  * Create release and publish Doxygen HTML documentation to a version
+    specific subdirectory in the `gh-pages` branch
+  * Publish the Doxygen for the *latest* (development) code to the `latest`
+    subdirectory in the gh-pages branch
+
+## Setup
+
+Create the `gh-pages` branch for a repository:
+```
+git checkout --orphan gh-pages
+git reset --hard
+touch .nojekyll
+git push origin gh-pages
+```
---
+++ include/test.hpp
@@ -0,0 +1,75 @@
+#ifndef DEPLOYMENTTEST_TEST_HPP
+#define DEPLOYMENTTEST_TEST_HPP
+
+#include <string>
+
+/**
+ * @file
+ *
+ * The main Deployment Test file
+ */
+
+/**
+ * Major version number of the Library
+ */
+#define DEPLOYTEST_VERSION_MAJOR    0
+/**
+ * Minor version number of the Library
+ */
+#define DEPLOYTEST_VERSION_MINOR    1
+/**
+ * Revision version number of the Library
+ */
+#define DEPLOYTEST_VERSION_REVISION 0
+/**
+ * Pre-release suffix of the Library
+ *
+ * Use [semantic versioning](https://semver.org/) suffices with alpha characters
+ * are compared by ASCII ordering; therefore `-DEV < -alpha < -beta < -rc`
+ * providing `DEV` is uppercase and `alpha`, `beta`, and `rc` are all lower case.
+ */
+#define DEPLOYTEST_VERSION_SUFFIX "-DEV"
+
+/**
+ * The Deployment Test namespace
+ */
+namespace deploytest
+{
+    /**
+     * The Deployment Test struct
+     */
+    class TestClass
+    {
+    public:
+        /**
+         * Creates an empty Test Class object
+         */
+        TestClass();
+
+        /**
+         * Creates a Test Class with the specified name
+         *
+         * @param name The name for the class
+         */
+        TestClass(const std::string& name);
+
+        /**
+         * Gets the name
+         *
+         * @return The name of the object
+         */
+        const std::string& Name() const { return m_name; }
+
+        /**
+         * Gets the object length
+         *
+         * @return The length of the object
+         */
+        std::size_t Length() const { return m_name.size(); }
+
+    private:
+        std::string m_name;
+    };
+}
+
+#endif // DEPLOYMENTTEST_TEST_HPP
