7007296b1a96a043e6b7040e1027c5751081d4ed
gridshadow <gridshadow@gmail.com>
2023-03-13 19:45:11 +0100

    Fixed sed for version number update

--- include/test.hpp
+++ include/test.hpp
@@ -16,7 +16,7 @@
 /**
  * Minor version number of the Library
  */
-#define DEPLOYTEST_VERSION_MINOR    9
+#define DEPLOYTEST_VERSION_MINOR    10
 /**
  * Revision version number of the Library
  */
