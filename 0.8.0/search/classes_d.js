f1be31047011cb5f24e08524e5afc3e01fb1f0c3
gridshadow <gridshadow@users.noreply.github.com>
2023-03-13 18:10:15 +0000

    Release documentation: 0.8.0

---
+++ 0.8.0/search/classes_d.js
@@ -0,0 +1,23 @@
+var searchData=
+[
+  ['nano_2586',['nano',['https://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
+  ['nanoseconds_2587',['nanoseconds',['https://en.cppreference.com/w/cpp/chrono/duration.html',1,'std::chrono']]],
+  ['negate_2588',['negate',['https://en.cppreference.com/w/cpp/utility/functional/negate.html',1,'std']]],
+  ['negation_2589',['negation',['https://en.cppreference.com/w/cpp/types/negation.html',1,'std']]],
+  ['negative_5fbinomial_5fdistribution_2590',['negative_binomial_distribution',['https://en.cppreference.com/w/cpp/numeric/random/negative_binomial_distribution.html',1,'std']]],
+  ['nested_5fexception_2591',['nested_exception',['https://en.cppreference.com/w/cpp/error/nested_exception.html',1,'std']]],
+  ['new_5fdelete_5fresource_2592',['new_delete_resource',['https://en.cppreference.com/w/cpp/experimental/new_delete_resource.html',1,'std::experimental::pmr']]],
+  ['new_5fhandler_2593',['new_handler',['https://en.cppreference.com/w/cpp/memory/new/new_handler.html',1,'std']]],
+  ['nonesuch_2594',['nonesuch',['https://en.cppreference.com/w/cpp/experimental/nonesuch.html',1,'std::experimental']]],
+  ['normal_5fdistribution_2595',['normal_distribution',['https://en.cppreference.com/w/cpp/numeric/random/normal_distribution.html',1,'std']]],
+  ['not_5fequal_5fto_2596',['not_equal_to',['https://en.cppreference.com/w/cpp/utility/functional/not_equal_to.html',1,'std::not_equal_to'],['https://en.cppreference.com/w/cpp/utility/functional/ranges/not_equal_to.html',1,'std::ranges::not_equal_to']]],
+  ['nothrow_5ft_2597',['nothrow_t',['https://en.cppreference.com/w/cpp/memory/new/nothrow_t.html',1,'std']]],
+  ['null_5fmemory_5fresource_2598',['null_memory_resource',['https://en.cppreference.com/w/cpp/experimental/null_memory_resource.html',1,'std::experimental::pmr']]],
+  ['nullopt_5ft_2599',['nullopt_t',['https://en.cppreference.com/w/cpp/utility/optional/nullopt_t.html',1,'std']]],
+  ['nullptr_5ft_2600',['nullptr_t',['https://en.cppreference.com/w/cpp/types/nullptr_t.html',1,'std']]],
+  ['num_5fget_2601',['num_get',['https://en.cppreference.com/w/cpp/locale/num_get.html',1,'std']]],
+  ['num_5fput_2602',['num_put',['https://en.cppreference.com/w/cpp/locale/num_put.html',1,'std']]],
+  ['numeric_5flimits_2603',['numeric_limits',['https://en.cppreference.com/w/cpp/types/numeric_limits.html',1,'std']]],
+  ['numpunct_2604',['numpunct',['https://en.cppreference.com/w/cpp/locale/numpunct.html',1,'std']]],
+  ['numpunct_5fbyname_2605',['numpunct_byname',['https://en.cppreference.com/w/cpp/locale/numpunct_byname.html',1,'std']]]
+];
---
+++ 0.8.0/search/files_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['new_2930',['new',['https://en.cppreference.com/w/cpp/header/new.html',1,'']]],
+  ['numeric_2931',['numeric',['https://en.cppreference.com/w/cpp/header/numeric.html',1,'']]]
+];
