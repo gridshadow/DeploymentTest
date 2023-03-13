7dcf2be6c3e9664d142be346ef758dfae595ee5f
gridshadow <gridshadow@users.noreply.github.com>
2023-03-13 08:25:39 +0000

    Release documentation: 0.7.0

---
+++ 0.7.0/search/files_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['optional_2932',['optional',['https://en.cppreference.com/w/cpp/header/optional.html',1,'']]],
+  ['ostream_2933',['ostream',['https://en.cppreference.com/w/cpp/header/ostream.html',1,'']]]
+];
---
+++ 0.7.0/search/files_d.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['scoped_5fallocator_2939',['scoped_allocator',['https://en.cppreference.com/w/cpp/header/scoped_allocator.html',1,'']]],
+  ['set_2940',['set',['https://en.cppreference.com/w/cpp/header/set.html',1,'']]],
+  ['shared_5fmutex_2941',['shared_mutex',['https://en.cppreference.com/w/cpp/header/shared_mutex.html',1,'']]],
+  ['span_2942',['span',['https://en.cppreference.com/w/cpp/header/span.html',1,'']]],
+  ['sstream_2943',['sstream',['https://en.cppreference.com/w/cpp/header/sstream.html',1,'']]],
+  ['stack_2944',['stack',['https://en.cppreference.com/w/cpp/header/stack.html',1,'']]],
+  ['stdexcept_2945',['stdexcept',['https://en.cppreference.com/w/cpp/header/stdexcept.html',1,'']]],
+  ['streambuf_2946',['streambuf',['https://en.cppreference.com/w/cpp/header/streambuf.html',1,'']]],
+  ['string_2947',['string',['https://en.cppreference.com/w/cpp/header/string.html',1,'']]],
+  ['string_5fview_2948',['string_view',['https://en.cppreference.com/w/cpp/header/string_view.html',1,'']]],
+  ['strstream_2949',['strstream',['https://en.cppreference.com/w/cpp/header/strstream.html',1,'']]],
+  ['syncstream_2950',['syncstream',['https://en.cppreference.com/w/cpp/header/syncstream.html',1,'']]],
+  ['system_5ferror_2951',['system_error',['https://en.cppreference.com/w/cpp/header/system_error.html',1,'']]]
+];
