8b7460980f60be9aed4c812824f1b9b14adb329b
gridshadow <gridshadow@users.noreply.github.com>
2023-03-13 18:48:37 +0000

    Release documentation: 0.10.0

---
+++ 0.10.0/navtreedata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Deployment Test", "index.html", [
+    [ "Deployment test", "index.html#autotoc_md0", [
+      [ "Setup", "index.html#autotoc_md1", null ],
+      [ "TODO", "index.html#autotoc_md2", null ]
+    ] ],
+    [ "Change Log", "md_CHANGELOG.html", [
+      [ "0.10.0", "md_CHANGELOG.html#autotoc_md4", null ],
+      [ "Change Log", "md_CHANGELOG.html#autotoc_md5", [
+        [ "0.9.0", "md_CHANGELOG.html#autotoc_md6", null ]
+      ] ],
+      [ "Change Log", "md_CHANGELOG.html#autotoc_md7", [
+        [ "0.8.0", "md_CHANGELOG.html#autotoc_md8", null ]
+      ] ],
+      [ "Change Log", "md_CHANGELOG.html#autotoc_md9", [
+        [ "0.7.0", "md_CHANGELOG.html#autotoc_md10", null ]
+      ] ],
+      [ "Change Log", "md_CHANGELOG.html#autotoc_md11", [
+        [ "0.6.0", "md_CHANGELOG.html#autotoc_md12", null ],
+        [ "0.1.0", "md_CHANGELOG.html#autotoc_md13", [
+          [ "Known Issues", "md_CHANGELOG.html#autotoc_md14", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"cpp/experimental/type_trait_variable_templates.html",
+"cpp/memory/destroy_at.html",
+"cpp/string/byte/strtok.html",
+"md_CHANGELOG.html#autotoc_md6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
